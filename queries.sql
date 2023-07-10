e2c9d016488e1c7107014f25495b643b45a5da6f
baqar-abbas <baqar007@hotmail.com>
2023-07-10 22:15:14 +0500

    Database created vet_clinic Animals table created
    Schema saved in Schema.sql, Insert queries added in data.sql and select queries added in queries.sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE name LIKE '%mon';
+SELECT * FROM animals WHERE date_of_birth > '2015-12-31' AND date_of_birth < '2020-01-01';
+SELECT * FROM animals WHERE nuetered = TRUE AND escape_attempts < 3;
+SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE nuetered = TRUE;
+SELECT * FROM animals WHERE name != 'Gabumon';
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
+CREATE DATABASE vet_clinic;
+
 CREATE TABLE animals (
-    name varchar(100)
-);
+id BIGSERIAL PRIMARY KEY NOT NULL,
+name VARCHAR(50) NOT NULL,
+date_of_birth DATE NOT NULL,
+escape_attempts INT NOT NULL,
+nuetered BOOLEAN NOT NULL,
+weight_kg decimal NOT NULL
+ );
